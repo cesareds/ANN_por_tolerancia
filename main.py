af44cd701b3ff345cda8d38f56d292b7ee4f287e
César Eduardo de Souza <cesarsouza.2002@icloud.com>
2023-03-18 16:21:18 -0300

    initial commit in new

--- main.py
+++ main.py
@@ -2,26 +2,16 @@ from sympy import *
 import numpy as np
 
 
-def newton(f, df, x0, n, iterations):
-    iter = 1
-    i2 = 0
-    for i in range(n+1):
-
+def newton(f, df, x0, n):
+    for i in range(5):
         dfx0 = df(x0)
         if dfx0 == 0:
             print("derivative == 0")
             break
-
         xi = x0 - f(x0) / dfx0
-
-        if iterations.__contains__(i):
-            print("AAAAA:\t" +  str(xi))
-            print(xi)
         x0 = xi
-
+        print("%.7f"%xi)
         print("i: "+ str(i) + "\tx0: " + str(x0) + "\tdfx0: " + str(dfx0) + "\tfx0: " + str(f(x0)) + "")
-
-iterations = {1, 3, 5}
 
 
 def f(x):
@@ -34,4 +24,4 @@ def df(x):
 
 x0 = 0.23216
 n = 5
-newton(f, df, x0, n, iterations)
+newton(f, df, x0, n)
